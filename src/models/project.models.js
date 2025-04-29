b2145eb380ebbcb7b792c394547e9c600eb44ce0
Sagar Maheshwari <maheshwarisagar65@gmail.com>
2025-04-29 15:56:11 +0530

    Add changes: robust project model changes

--- src/models/project.models.js
+++ src/models/project.models.js
@@ -1,4 +1,9 @@
 import mongoose, { Schema } from "mongoose";
+import {
+  ProjectStatusEnum,
+  ProjectPriorityEnum,
+  ProjectVisibilityEnum,
+} from "../utils/constants.js";
 
 const projectSchema = new Schema(
   {
@@ -12,6 +17,34 @@ const projectSchema = new Schema(
       type: String,
       required: true,
     },
+    status: {
+      type: String,
+      enum: Object.values(ProjectStatusEnum),
+      default: ProjectStatusEnum.ACTIVE,
+    },
+    priority: {
+      type: String,
+      enum: Object.values(ProjectPriorityEnum),
+      default: ProjectPriorityEnum.MEDIUM,
+    },
+    startDate: {
+      type: Date,
+      default: Date.now,
+    },
+    dueDate: {
+      type: Date,
+    },
+    visibility: {
+      type: String,
+      enum: Object.values(ProjectVisibilityEnum),
+      default: ProjectVisibilityEnum.PRIVATE,
+    },
+    tags: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
     createdBy: {
       type: Schema.Types.ObjectId,
       ref: "User",
@@ -19,6 +52,14 @@ const projectSchema = new Schema(
     },
   },
   { timestamps: true },
+);
+
+projectSchema.index(
+  {
+    name: 1,
+    createdBy: 1,
+  },
+  { unique: true },
 );
 
 const Project = mongoose.model("Project", projectSchema);
